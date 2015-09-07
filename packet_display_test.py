6c804ff0b8f578464b704afda90522032c478f23
Yikai <yklin@umich.edu>
2015-09-06 21:09:14 -0400

    created packet_display_test.py to test scapy functionalities

---
+++ packet_display_test.py
@@ -0,0 +1,20 @@
+from scapy.automaton import *
+
+
+class HelloWorld(Automaton):
+    @ATMT.state(initial=1)
+    def BEGIN(self):
+        print "State=BEGIN"
+
+    @ATMT.condition(BEGIN)
+    def wait_for_nothing(self):
+        print "Wait for nothing..."
+        raise self.END()
+
+    @ATMT.action(wait_for_nothing)
+    def on_nothing(self):
+        print "Action on 'nothing' condition"
+
+    @ATMT.state(final=1)
+    def END(self):
+        print "State=END"
